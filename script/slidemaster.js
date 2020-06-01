170afcd575baf7ba27d1527c471591c38f35ae31
ecclisia <drop4444@naver.com>
2020-06-01 10:54:39 +0900

    1차_2

---
+++ script/slidemaster.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+
+    var num = 0;
+
+
+    $(".nextBtn").on("click", function () {
+
+
+
+        if (num < 4) {
+            num = num + 1;
+        } else {
+            num = 0;
+        }
+
+        $(".slide li").eq(num).css({
+
+            "left": "100%"
+        }).show();
+
+        $(".slide li").eq(num - 1).animate({
+            "left": "-100%"
+        }, 500);
+
+        $(".slide li").eq(num).animate({
+            "left": "0px"
+        }, 500);
+
+        $(".slideControl li").removeClass("cur");
+        $(".slideControl li").eq(num).addClass("cur");
+
+    });
+
+
+    $(".prevBtn").click(function () {
+
+
+        if (num > 0) {
+            num = num - 1;
+        } else {
+            num = 4;
+        }
+
+        $(".slide li").eq(num).css({
+
+            "left": "-100%"
+        }).show();
+
+        if (num == 4) {
+            var curNum = 0;
+        } else {
+            var curNum = num + 1;
+        }
+        $(".slide li").eq(curNum).animate({
+            "left": "100%"
+        }, 500);
+
+        $(".slide li").eq(num).animate({
+            "left": "0px"
+        }, 500);
+
+        $(".slideControl li").removeClass("cur");
+        $(".slideControl li").eq(num).addClass("cur");
+
+    });
+
+
+
+    $(".slideControl li").click(function () {
+
+        var indexNum = $(".slideControl li").index(this);
+
+        if (num < indexNum) {
+
+            $(".slide li").eq(indexNum).css("left", "100%").show();
+
+            $(".slide li").eq(num).show().animate({
+                "left": "-100%"
+            }, 500);
+
+            $(".slide li").eq(indexNum).animate({
+                "left": "0px"
+            }, 500);
+
+
+
+            num = indexNum;
+
+
+        } else if (num > indexNum) {
+
+            $(".slide li").eq(indexNum).css("left", "-100%").show();
+
+            $(".slide li").eq(num).show().animate({
+                "left": "100%"
+            }, 500);
+
+            $(".slide li").eq(indexNum).animate({
+                "left": "0px"
+            }, 500);
+
+            num = indexNum;
+
+        } else {
+            $(".slide li").eq(indexNum).css("left", "0px").show();
+
+
+        }
+
+        $(".slideControl li").removeClass("cur");
+        $(".slideControl li").eq(indexNum).addClass("cur");
+
+   
+	});
+	});
